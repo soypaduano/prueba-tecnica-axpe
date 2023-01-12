c03c1d24058e3bbfd8a1c3e5a9409ee992c14c47
soypaduano <sebastian.paduano.m@gmail.com>
2023-01-12 17:59:46 +0100

    Header hiding when scrolling down

--- react-meetup/src/components/layout/MainNavigation.js
+++ react-meetup/src/components/layout/MainNavigation.js
@@ -1,16 +1,15 @@
 import { ALL_MEETUP_PAGE, FAVORITES_PAGE, NEW_MEETUP_PAGE } from "./../../utils/constants";
 
-import classes from "./MainNavigation.css";
+import classes from "./MainNavigation.module.css"
 import useScrollDirection from './useScrollDirection';
 
 export default function MainNavigation({ setPage }) {
 
   const scrollDirection = useScrollDirection();
-  console.log(scrollDirection);
 
   return (
-    <header className={`header ${ scrollDirection === "down" ? "hide" : "show"}`} data-test="navigation-header">
-      <div className='logo'>React Meetups</div>
+    <header className={`${classes.header} ${scrollDirection === "down" ? classes.down : ""}`} data-test="navigation-header">
+      <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
           <li>
@@ -27,11 +26,12 @@ export default function MainNavigation({ setPage }) {
           <li>
             <a href="#" onClick={() => setPage(FAVORITES_PAGE)}>
               My Favorites
-              <span className='badge'>{0}</span>
+              <span className={classes.badge}>{0}</span>
             </a>
           </li>
         </ul>
       </nav>
     </header>
+    
   );
 }

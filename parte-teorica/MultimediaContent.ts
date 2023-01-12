986f8e4ef77a2e4c48527b35f14c33021b1f2b39
soypaduano <sebastian.paduano.m@gmail.com>
2023-01-12 13:00:36 +0100

    Parte teorica

---
+++ parte-teorica/MultimediaContent.ts
@@ -0,0 +1,37 @@
+
+interface MultimediaContent {
+    title: string;
+    price: number;
+    duration: number;
+    adult: boolean;
+    additionalFee: number;
+  }
+
+
+class MultimediaContentDownload implements MultimediaContent {
+    title: string;
+    price: number;
+    duration: number;
+    adult: boolean;
+    size: number;
+    additionalFee: number;
+}
+
+class MultimediaContentStream implements MultimediaContent {
+    title: string;
+    price: number;
+    duration: number;
+    adult: boolean;
+    additionalFee: number;
+}
+
+class MultimediaContentPodcast implements MultimediaContent {
+    title: string;
+    price: number;
+    duration: number;
+    adult: boolean;
+    size: number;
+    additionalFee: number;
+}
+
+
---
+++ parte-teorica/RegisteredUser.js
@@ -0,0 +1,56 @@
+/*Mejoras:
+    1. Para empezar creo que toda la lógica de calculo de precio no debería ir dentro de RegisteredUser, ya que forma parte del servicio. 
+       Para eso, dentro de Service, crearía otro metodo que se llame getTotalOfUser(id). 
+    2. El if / else no es nada escalable: tendremos que añadir uno a uno cada if si queremos escalar la aplicación con distintos tipos de
+       contenido multimedia que vaya surgiendo.  
+       Por eso propongo, pasar a una interfaz que se llame MultimediaContent y los demás MultimediaContent que se vayan creando,
+       extiendan esa interfaz y tengan el atributo de price (entre otros). He creado otro contenido multimedia, como podcast,
+       para que se entienda la escalabilidad de esto.  
+*/
+
+class RegisteredUser {
+    constructor(services = []){
+        this.id = "xxx"
+        this.services = services;
+        this.email = "sebastian.paduano.m@gmail.com"
+        this.password = "1234"
+        this.registration = '12-02-2022'
+        this.adult = true;
+    }
+
+    
+    getTotal(){
+        //Movemos la lógica al servicio, ya que el calculo de precio pertenece al servicio.
+        return Service.getTotalOfUser(this.id) 
+    }
+}
+
+class Service {
+    constructor(timestamp){
+        this.timestamp = timestamp;
+    }
+
+    getMultimediaContent(userId){
+        return this.getMultimediaContentInDatabaseOfUser(userId);
+    }
+
+    getTotalOfUser(userId){
+        //let multimediaContent = this.getMultimediaContent(userId); //Esto devuelve todos el contenido que tiene el usuario
+        let multimediaContent = [10, 20, 30, 1, 2, 5, 6, 78, 10, 1, 0, 0]; 
+        const total = multimediaContent.reduce((partialSum, content) => {
+            /*
+                Con esto evitamos el if/else -> Todos los contenidos tienen un precio, sin importar su tipo (puede ser 0)
+                Además, también contemplamos que si es premium, le metemos directamente el additionalFee. 
+            */
+            return partialSum + content.price + (content.isPremium ? content.additionalFee : 0);
+        }, 0);
+
+        return total;
+    }
+    getTotalOfUser()
+}
+
+/* 
+    Pero para que esto funcione, deberíamos implementar una interfaz llamada MultimediaContent. 
+    Ya que JS no tiene features de OOP como tal, haré el pseudocódigo en TS. (Mirar archivo de MultimediaContent)
+*/
